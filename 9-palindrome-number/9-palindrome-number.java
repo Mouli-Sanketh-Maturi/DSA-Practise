6d71003e1a2eaf053bac2df0dd3b7c606d4ffdc3
Sanketh-Maturi <sanketh789@gmail.com>
2022-08-19 22:38:11 +0530

    Time: 17 ms (34.52%), Space: 45.3 MB (27.36%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
         String num = String.valueOf(x);
         char [] numChars = num.toCharArray();
         for(int i = 0; i < numChars.length; i++){
