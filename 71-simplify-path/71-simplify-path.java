5d1eb8cf0eeae3348be6938df1a5551b566341bf
Sanketh-Maturi <sanketh789@gmail.com>
2022-08-20 10:27:32 +0530

    Time: 12 ms (33.25%), Space: 45 MB (19.88%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,7 +1,6 @@
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
-        Stack<String> revStack = new Stack<>();
         String canPath = "";
         String [] arr = path.split("/",0);
         for(String dir : arr){
@@ -14,11 +13,8 @@ class Solution {
                 stack.push(dir);
             }
         }
-        while(!stack.empty()){
-            revStack.push(stack.pop());
-        }
-        while(!revStack.empty()){
-            canPath = canPath.concat("/"+revStack.pop());
+        for(String dir: stack){
+            canPath = canPath.concat("/"+dir);
         }
         if(canPath.isEmpty()){
             return "/";
