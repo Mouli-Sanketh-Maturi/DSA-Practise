c361703ea546b3f202c1869e0b91d7400cde1403
Sanketh-Maturi <sanketh789@gmail.com>
2022-09-14 23:02:13 +0530

    Time: 2 ms (19.23%), Space: 41.7 MB (31.04%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        int [] dp = new int [nums.length];
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         if(nums.length == 1) {
             return nums[0];
         } else if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        dp[0] = nums[0];
-        dp[1] = nums[1];
         for(int i=2; i<nums.length; i++){
-            queue.add(dp[i-2]);
-            dp[i] = queue.peek() + nums[i];
+            queue.add(nums[i-2]);
+            nums[i] += queue.peek();
         }
-        return Math.max(dp[nums.length-1], dp[nums.length-2]);
+        return Math.max(nums[nums.length-1], nums[nums.length-2]);
     }
 }
