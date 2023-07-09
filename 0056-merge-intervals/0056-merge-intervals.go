bba1f98cacb96a195b7080e58eabaf1f90912985
Mouli Sanketh Maturi <sanketh789@gmail.com>
2023-07-09 23:45:43 +0530

    Time: 22 ms (75.56%), Space: 7.1 MB (35.66%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,43 @@
+func merge(intervals [][]int) [][]int {
+
+	size := len(intervals)
+
+	if size <= 1 {
+		return intervals
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	newInterval := intervals[0]
+
+	mergedInterval := [][]int{}
+
+	for i := 1; i < size; i++ {
+		if intervals[i][0] <= newInterval[1] {
+			newInterval[0] = min(newInterval[0], intervals[i][0])
+			newInterval[1] = max(newInterval[1], intervals[i][1])
+		} else {
+			mergedInterval = append(mergedInterval, newInterval)
+			newInterval = intervals[i]
+		}
+	}
+
+	mergedInterval = append(mergedInterval, newInterval)
+	return mergedInterval
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
