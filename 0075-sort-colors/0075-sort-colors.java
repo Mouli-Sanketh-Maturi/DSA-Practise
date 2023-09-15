d9a31e29b001ab84b264b1caa0e1e4a03c881d3a
Mouli Sanketh Maturi <sanketh789@gmail.com>
2023-09-15 18:00:50 -0400

    Time: 0 ms (100%), Space: 40.4 MB (99.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count1 = 0,count2 = 0,count0 = 0;
+        // Dutch National Flag
+        // 0 ...... low-1 -> 0 Extreme Left
+        // low .... mid-1 -> 1
+        // high+1 ... n-1 -> 2 Extreme Right
+        // mid ..... high -> Unsorted
 
-        for (int num : nums) {
-            if (num == 1) {
-                count1++;
-            } else if (num == 2) {
-                count2++;
+        int low = 0,mid = 0,high = nums.length-1;
+
+        while (mid <= high) {
+            if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
             } else {
-                count0++;
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
 
-        int i = 0;
-
-        while(i<count0) {
-            nums[i++] = 0;
-        }
-
-        while(i<count1+count0) {
-            nums[i++] = 1;
-        }
-
-        while(i<count0+count1+count2) {
-            nums[i++]=2;
-        }
     }
 }
