cb8d4cc203cdee504bfffdbef4c58db378a4b15f
Mouli Sanketh Maturi <sanketh789@gmail.com>
2024-05-31 14:16:29 -0400

    Time: 5 ms (89.55%), Space: 45.8 MB (17.58%) - LeetHub

--- 1624-clone-binary-tree-with-random-pointer/1624-clone-binary-tree-with-random-pointer.java
+++ 1624-clone-binary-tree-with-random-pointer/1624-clone-binary-tree-with-random-pointer.java
@@ -23,20 +23,6 @@ class Solution {
         }
         Map<Node,NodeCopy> nodeMap = new HashMap<>();
         NodeCopy newRoot = copyNode(root, nodeMap);
-        Queue<Node> nodeQueue = new LinkedList<Node>();
-        nodeQueue.offer(root);
-        while(!nodeQueue.isEmpty()) {
-            Node ptr = nodeQueue.poll();
-            if(ptr.random != null) {
-                nodeMap.get(ptr).random = nodeMap.get(ptr.random);
-            }
-            if(ptr.left != null) {
-                nodeQueue.offer(ptr.left);
-            }
-            if(ptr.right != null) {
-                nodeQueue.offer(ptr.right);
-            }
-        }
         return newRoot;
     }
 
@@ -46,18 +32,11 @@ class Solution {
         } else if(nodeMap.containsKey(root)) {
             return nodeMap.get(root);
         }
-        NodeCopy leftNode = null;
-        NodeCopy rightNode = null;
-        if(root.left != null) {
-            leftNode = copyNode(root.left, nodeMap);
-            nodeMap.put(root.left, leftNode);
-        }
-        if(root.right != null) {
-            rightNode = copyNode(root.right, nodeMap);
-            nodeMap.put(root.right, rightNode);
-        }
-        NodeCopy newNode = new NodeCopy(root.val,leftNode ,rightNode , null);
+        NodeCopy newNode = new NodeCopy(root.val);
         nodeMap.put(root, newNode);
+        newNode.left = copyNode(root.left, nodeMap);
+        newNode.right = copyNode(root.right, nodeMap);
+        newNode.random = copyNode(root.random, nodeMap);
         return newNode;
     }
 }
