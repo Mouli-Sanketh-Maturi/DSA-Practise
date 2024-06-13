4fb3361e1aef8c443d3d8bd0c93460f3ef03912d
Mouli Sanketh Maturi <sanketh789@gmail.com>
2024-06-13 00:46:59 -0400

    Time: 53 ms (5.1%), Space: 45.4 MB (15.18%) - LeetHub

---
+++ 0965-unique-email-addresses/0965-unique-email-addresses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        return (int) Arrays.stream(emails).map( email -> convert(email)).distinct().count();
+    }
+
+    public static String convert(String email) {
+        String[] arr = email.split("@");
+        arr[0] = arr[0].replaceAll("\\.","");
+        arr[0] = arr[0].split("\\+")[0];
+        System.out.println(arr[0]+arr[1]);
+        return arr[0] + "@" + arr[1];
+    }
+}
