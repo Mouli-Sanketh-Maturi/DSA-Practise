a17365fc3e0e5a824162858f43df2581cfa5d2c2
Mouli Sanketh Maturi <sanketh789@gmail.com>
2024-06-21 17:38:41 -0400

    Time: 8 ms (24.24%), Space: 45.9 MB (29.89%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> prerequisiteParentMapping = new HashMap<>();
+        int[] prerequisiteFrequency = new int[numCourses];
+        int[] finishOrder = new int[numCourses];
+        for(int[] prerequisite : prerequisites) {
+            List<Integer> prerequisiteSet = prerequisiteParentMapping.getOrDefault(prerequisite[1], new ArrayList<>());
+            prerequisiteSet.add(prerequisite[0]);
+            prerequisiteParentMapping.put(prerequisite[1], prerequisiteSet);
+            prerequisiteFrequency[prerequisite[0]]++;
+        }
+
+        Queue<Integer> finishableCourses = new LinkedList<>();
+        for(int i=0; i<prerequisiteFrequency.length; i++) {
+            if(prerequisiteFrequency[i] == 0) {
+                finishableCourses.add(i);
+            }
+        }
+
+        int finishedCourseCount = 0;
+
+        while(!finishableCourses.isEmpty()) {
+            int courseNumber = finishableCourses.poll();
+            finishOrder[finishedCourseCount++] = courseNumber;
+            for(int prerequisiteParentCourseNumber : prerequisiteParentMapping.getOrDefault(courseNumber, new ArrayList<>())) {
+                prerequisiteFrequency[prerequisiteParentCourseNumber]--;
+                if(prerequisiteFrequency[prerequisiteParentCourseNumber] == 0) {
+                    finishableCourses.add(prerequisiteParentCourseNumber);
+                }
+            }
+        }
+
+        System.out.println(finishedCourseCount);
+
+        if(finishedCourseCount < numCourses) { return new int[0];}
+
+        return finishOrder;
+        
+    }
+}
